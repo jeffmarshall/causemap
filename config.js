6f55a2ec50d18e4f64ad5c915597e8ad31c6e179
Jeff Marshall <jeff@classy.co>
2013-11-01 17:20:53 +0000

    Starting over yet again.
    So, it's been a while that this repo has existed. The first commit in the log
    is from two years ago, and I'm guessing that this repo housed the first
    html/css mockups of the interface. A lot has happened since I started this
    project, but the most important thing to talk about here would be where this
    specific project is going.
    
    Causemap, the web app that allows users to link situations by cause and effect,
    has been split into three code projects:
    
    1. **Cartography**: Low-level database and ORM library. It's main offering is a
    library of basic models.
    
    2. **Causemap**: Community-centric library that depends and extends on
    Cartography. Users, comments, actions, and search ranking based on user
    activity (search may at some point be refactored into another library).
    
    3. **Causemap.org**: The web app that relies on the Causemap library. It's what
    will be served by causemap.org.
    
    This is the plan for now.

---
+++ config.js
@@ -0,0 +1,26 @@
+var _ = require('lodash');
+
+
+
+module.exports = (function(){
+  try {
+    var configs = require('./settings');
+  } catch (e){
+    var configs = {};
+  }
+
+  return {
+    set: function(){
+      switch(arguments.length){
+        case 1: _.extend(configs, arguments[0]); return arguments[0];
+        case 2: configs[arguments[0]] = arguments[1]; return arguments[1];
+      }
+    },
+    get: function(key){
+      if (key === undefined){
+        return _.clone(configs);
+      }
+      return configs[key];
+    }
+  }
+})();
