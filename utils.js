760d8c5e96897bd7a71056e41a2974c314b81308
Jeff Marshall <jeff@classy.co>
2013-11-21 22:54:10 -0500

    Getting rid of redundant utils.

--- utils.js
+++ utils.js
@@ -1,30 +1,3 @@
-var ops = require('./ops');
-
-
-
-
-function getHeaders(id, callback){
-  var nano = ops.initNano();
-
-  request_options = {
-    db: config.get('dbname'),
-    doc: id,
-    method: 'HEAD'
-  }
-
-  nano.request(request_options, function(request_error, result, headers){
-    callback(request_error, headers);
-  });
-}
-
-
-function exists(id, callback){
-  getHeaders(id, function(headers_error, headers){
-    callback(headers_error, headers ? headers['status-code'] === 200 : null);
-  });
-}
-
-
 function slugify(text) {
   text = text.replace(/[^-a-zA-Z0-9,&\s]+/ig, '');
   text = text.replace(/-/gi, "_");
@@ -35,7 +8,5 @@ function slugify(text) {
 
 
 module.exports = {
-  slugify: slugify,
-  headers: getHeaders,
-  exists: exists
+  slugify: slugify
 }
