4f92845f1808162b0bb4927b987d903eebfe6603
Jeff Marshall <jeff@classy.co>
2013-12-02 13:29:56 -0500

    Deleting bookmarks in user.delete()

--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ var util = require('util');
 var cartography = require('cartography');
 var Bookmark = require('./bookmark');
 
+var db = require('./db').db;
+
 
 
 
@@ -53,6 +55,52 @@ User.prototype.unbookmark = function deleteBookmarkForUser(
   ].join(':'));
 
   return bookmark.delete(callback)
+}
+
+
+
+User.prototype.bookmarks = function totalBookmarksForUser(
+  callback
+){
+  var self = this;
+
+  var view_options = {
+    include_docs: true,
+    startkey: [ self.id ],
+    endkey: [ self.id, {} ],
+    reduce: false
+  }
+
+  db().view(
+    'bookmarks',
+    'by_user',
+    view_options,
+    function(view_error, view_result){
+      if (view_error) return callback(view_error, null);
+      return callback(null, view_result.rows.map(
+        function(row){ return row.doc }
+      ))
+    }
+  )
+}
+
+
+
+User.prototype.delete = function deleteUser(callback){
+  var self = this;
+
+  // delete bookmarks
+  self.bookmarks(function(error, result){
+    if (error) return callback(error, null);
+
+    db().bulk({ docs: result.map(function(doc){
+      doc._deleted = true;
+      return doc;
+    }) }, function(bulk_error, bulk_delete){
+      if (bulk_error) return callback(bulk_error, null);
+      User.super_.prototype.delete.call(self, callback);
+    })
+  })
 }
 
 
