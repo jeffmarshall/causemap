81e93195542c199ae4a16a881cc936ed3179e1ea
Jeff Marshall <jeff@classy.co>
2013-12-04 11:40:23 -0500

    Relationship indexing includes strength.
    and excludes cruft: _rev, immutable, revisable, etc.

--- search/update/relationship.js
+++ search/update/relationship.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+var async = require('async');
 var config = require('../../config');
 var Relationship = require('cartography').models.Relationship;
 
@@ -14,30 +15,56 @@ module.exports = function updateOperationsForRelationship(
 ){
   var relationship = new Relationship(doc._id);
 
-  relationship.summarize(function(summarization_error, relationship_summary){
-    if (summarization_error){
-      return callback(summarization_error, null);
+  async.parallel([
+    function(parallel_callback){
+      relationship.summarize(function(
+        summarization_error, 
+        relationship_summary
+      ){
+        if (summarization_error){
+          return parallel_callback(summarization_error, null);
+        }
+
+        var cause = relationship_summary.cause;
+        var effect = relationship_summary.effect;
+
+        [relationship_summary, cause, effect].forEach(function(doc){
+          delete doc._rev;
+          delete doc.immutable;
+          delete doc.revisable;
+        });
+
+        relationship_summary.cause = cause;
+        relationship_summary.effect = effect;
+        
+        return parallel_callback(null, relationship_summary)
+      });
+    },
+
+    function(parallel_callback){
+      relationship.strength(function(strength_error, strength_rating){
+        if (strength_error) return parallel_callback(strenght_error, null);
+        return parallel_callback(null, { strength: strength_rating })
+      });
     }
+  ], function(parallel_error, parallel_results){
+    if (parallel_error) return callback(parallel_error, null);
 
-    var relationship_summary_without_description = _.clone(
-      relationship_summary
-    )
+    var summary = {};
 
-    delete relationship_summary_without_description.description;
+    parallel_results.forEach(function(result){
+      summary = _.extend(summary, result);
+    });
 
     return callback(null, [
-      {index: {
-        index: main_index_name,
-        type: doc.type,
-        id: doc._id,
-        data: relationship_summary
-      }},
-      {index: {
-        index: suggestion_index_name,
-        type: doc.type,
-        id: doc._id,
-        data: relationship_summary_without_description
-      }},
+      {
+        index: {
+          index: main_index_name,
+          type: doc.type,
+          id: doc._id,
+          data: summary
+        }
+      }
     ]);
   });
 }
