64cef171205e9cfd7a89ff40e63581041270923d
Jeff Marshall <jeff@classy.co>
2013-12-03 21:39:58 -0500

    Adding popularity and controversiality to situation indexing.

--- search/update/situation.js
+++ search/update/situation.js
@@ -16,6 +16,8 @@ module.exports = function updateOperationsForSituation(
   var situation = new Situation(doc._id);
   var summary;
   var body;
+  var popularity;
+  var controversiality;
 
   async.parallel([
     function(parallel_callback){
@@ -36,9 +38,40 @@ module.exports = function updateOperationsForSituation(
         summary = situation_summary;
         return parallel_callback(null, summary);
       });
+    },
+    
+    function(parallel_callback){
+      situation.popularity(function(popularity_error, popularity_rating){
+        if (popularity_error) return parallel_callback(popularity_error, null);
+        popularity = popularity_rating;
+        return parallel_callback(null, popularity_rating);
+      })
+    },
+    
+    function(parallel_callback){
+      situation.controversiality(function(
+        controversiality_error, 
+        controversiality_rating
+      ){
+        if (controversiality_error){
+          return parallel_callback(controversiality_error, null);
+        }
+
+        controversiality = controversiality_rating;
+
+        return parallel_callback(null, controversiality_rating);
+      })
     }
   ], function(parallel_error, parallel_result){
     if (parallel_error){ return callback(parallel_error, null) }
+
+    [body, summary].forEach(function(doc){
+      delete doc._rev;
+      delete doc.immutable;
+      delete doc.revisable;
+      doc.popularity = popularity;
+      doc.controversiality = controversiality;
+    });
 
     return callback(null, [
       {index: {
